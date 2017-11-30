18425ce4467b4a28ef776d2987cb8ffed7e1e274
Hiroki Ito <contact@hekki.info>
2017-12-01 01:02:50 +0900

    add response today scedule

--- bot/respond.py
+++ bot/respond.py
@@ -2,7 +2,24 @@ from slackbot.bot import respond_to
 from bot.schedule import Scedule
 
 
-@respond_to('ゴミ|ごみ')
+@respond_to(r'^.*今日.*ごみ.*$')
+@respond_to(r'^.*今日.*ゴミ.*$')
+def mention_func(message):
+    from datetime import date, datetime
+    today_weekday = datetime.today().weekday()
+
+    scedule = Scedule()
+    if scedule.of_weekday(today_weekday) is None:
+        message.reply('今日は何も出せないよ！')
+    else:
+        today_scedule = ', '.join(scedule.of_weekday(today_weekday))
+        message.reply(f"{'今日のゴミは' + ' *' + today_scedule + '* ' + 'だよ！'}")
+
+
+@respond_to(r'^.*明日.*ごみ.*$')
+@respond_to(r'^.*明日.*ゴミ.*$')
+@respond_to(r'^ごみ$')
+@respond_to(r'^ゴミ$')
 def mention_func(message):
     from datetime import date, datetime
     tomorrow_weekday = datetime.today().weekday() + 1
