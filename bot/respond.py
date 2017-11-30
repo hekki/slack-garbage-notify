0811566521dd68619c5e7ad11e2c12b8965b9d98
Hiroki Ito <contact@hekki.info>
2017-12-01 02:38:54 +0900

    support unburnable day judgment

--- bot/respond.py
+++ bot/respond.py
@@ -6,13 +6,12 @@ from bot.schedule import Scedule
 @respond_to(r'^.*今日.*ゴミ.*$')
 def mention_func(message):
     from datetime import date
-    today_weekday = date.today().weekday()
 
     scedule = Scedule()
-    if scedule.of_weekday(today_weekday) is None:
+    if scedule.of_date(date.today()) is None:
         message.reply('今日は何も出せないよ！')
     else:
-        today_scedule = ', '.join(scedule.of_weekday(today_weekday))
+        today_scedule = ', '.join(scedule.of_date(date.today()))
         message.reply(f"{'今日のゴミは' + ' *' + today_scedule + '* ' + 'だよ！'}")
 
 
@@ -23,11 +22,10 @@ def mention_func(message):
 def mention_func(message):
     from datetime import date, timedelta
     tomorrow = date.today() + timedelta(days=1)
-    tomorrow_weekday = tomorrow.weekday()
 
     scedule = Scedule()
-    if scedule.of_weekday(tomorrow_weekday) is None:
+    if scedule.of_date(tomorrow) is None:
         message.reply('明日は何も出せないよ！')
     else:
-        tomorrow_scedule = ', '.join(scedule.of_weekday(tomorrow_weekday))
+        tomorrow_scedule = ', '.join(scedule.of_date(tomorrow))
         message.reply(f"{'明日のゴミは' + ' *' + tomorrow_scedule + '* ' + 'だよ！'}")
--- bot/schedule.py
+++ bot/schedule.py
@@ -14,5 +14,17 @@ class Scedule:
                          [TYPE['burnable']],
                          None]
 
+    def of_date(self, date):
+        from math import ceil
+        week_of_month = ceil(date.day / 7)
+        weekday = date.weekday()
+
+        if (week_of_month == 1 or week_of_month == 3) and (weekday == 1):
+            sp_schedule = self.SCHEDULE[weekday].copy()
+            sp_schedule.append(TYPE['unburnable'])
+            return sp_schedule
+        else:
+            return self.SCHEDULE[weekday]
+
     def of_weekday(self, weekday):
         return self.SCHEDULE[weekday]
